58605905cfcb1edb5ad96ef36a8331617f6d9b39
Fabrice Reigner <fabricereigner49@msn.com>
2020-09-14 14:58:40 +0200

    First commit
    Add all project

---
+++ Assets/Scripts/LoadSpecificScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSpecificScene : MonoBehaviour
+{
+    public string SceneName;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneName);
+
+        }
+    }
+}
