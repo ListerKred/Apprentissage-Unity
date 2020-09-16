3603c246e037450fb29343ca8fb1d40a7547aa7b
Fabrice Reigner <fabricereigner49@msn.com>
2020-09-16 15:00:18 +0200

    Add heal
    The player can heal when he loss hp

---
+++ Assets/Scripts/HealPowerUp.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class HealPowerUp : MonoBehaviour
+{
+    public int healthPoints;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //regarde si les pv sont au max ou non
+            if (PlayerHealth.instance.currentHealth != PlayerHealth.instance.maxHealth)
+            {
+                //rend la vie au joueur
+                PlayerHealth.instance.HealPlayer(healthPoints);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -12,6 +12,17 @@ public class PlayerHealth : MonoBehaviour
 
     public SpriteRenderer graphics;
     public HealthBar healthBar;
+    public static PlayerHealth instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de PlayerHealth dans la scène");
+            return;
+        }
+        instance = this;
+    }
     void Start()
     {
         currentHealth = maxHealth;
@@ -23,6 +34,20 @@ public class PlayerHealth : MonoBehaviour
         {
             TakeDamage(20);
         }
+    }
+
+    //Set les pv max a 100 et ajoute les Pv au joueur
+    public void HealPlayer (int amount)
+    {
+        if ((currentHealth + amount) > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else
+        {
+            currentHealth += amount;
+        }
+        healthBar.SetHealth(currentHealth);
     }
 
     public void TakeDamage(int damage)
